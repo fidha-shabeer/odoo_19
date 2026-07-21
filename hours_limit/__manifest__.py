e8ed1774a4a3fb54db29d769d145cc5c4aa2758f
fidha <fidhashabeer2002@gmail.com>
2026-07-21 17:33:59 +0530

    21/06/2026 [UPDT] xlsx report generation in recurring subscription

---
+++ hours_limit/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Hours Limit",
+    'summary': "Hours Limit",
+    'description': "Hours Limit",
+    'version': "19.0.1.0.0",
+    'category': "Project",
+    'author': "Cybrosys Technology",
+    'license': "LGPL-3",
+    'application': True,
+    'sequence' : -1,
+    'depends': ['base','project',],
+    'data': ["views/project_task.xml",
+             ],
+}
