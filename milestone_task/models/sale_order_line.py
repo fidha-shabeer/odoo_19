0c478f7be3e009ac5dddb5795bdaeae855e6f227
fidha <fidhashabeer2002@gmail.com>
2026-07-22 17:53:58 +0530

    22/06/2026 [UPDT] xlsx report generation in recurring subscription

--- milestone_task/models/sale_order_line.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models,fields,api
-class SaleOrderLine(models.Model):
-    _inherit = "sale.order.line"
-
-    milestone = fields.Integer(string="Milestone")
-
-
--- vip_discount_sales/models/res_partner.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import fields, models
-
-class ResPartner(models.Model):
-    _inherit = "res.partner"
-
-    is_vip = fields.Boolean(string="Is VIP")
-    vip_discount = fields.Float(string="VIP Discount")
-
